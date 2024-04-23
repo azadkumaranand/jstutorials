911de419138773efa24c15894b99c532673e264d
azadanand9798 <azadanand9798@gmail.com>
2024-04-23 18:00:08 +0530

    initial commit

---
+++ tut1.js
@@ -0,0 +1,23 @@
+let name = "azad";
+
+// console.log(name);
+
+//exponential operator
+let result = 2+4;
+
+//devision operator
+
+result = 5/2;
+
+//modulas operator
+
+result = 10%2;
+
+
+//incriment and decrement operator
+result++;
+result++;
+result--;
+
+console.log(result);
+
